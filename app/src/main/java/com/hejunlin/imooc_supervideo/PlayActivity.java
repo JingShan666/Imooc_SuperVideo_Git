19f1d07d25b85010d857ed6b2f4a21e02804c395
逆流的鱼yuiop <1007235554@qq.com>
2017-04-22 20:01:16 +0800

    1、移植ijkplayer到项目中(包含so)

--- app/src/main/java/com/hejunlin/imooc_supervideo/PlayActivity.java
+++ app/src/main/java/com/hejunlin/imooc_supervideo/PlayActivity.java
@@ -4,20 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.hejunlin.imooc_supervideo.base.BaseActivity;
 import com.hejunlin.imooc_supervideo.model.sohu.Video;
+import com.hejunlin.imooc_supervideo.widget.media.IjkVideoView;
 
-public class PlayActivity extends AppCompatActivity {
+import tv.danmaku.ijk.media.player.IjkMediaPlayer;
+
+public class PlayActivity extends BaseActivity {
 
     private static final String TAG = PlayActivity.class.getSimpleName();
     private String mUrl;
     private int mStreamType;
     private int mCurrentPosition;
     private Video mVideo;
+    private IjkVideoView mVideoView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play);
+    protected int getLayoutId() {
+        return R.layout.activity_play;
+    }
+
+    @Override
+    protected void initView() {
         mUrl = getIntent().getStringExtra("url");
         mStreamType = getIntent().getIntExtra("type", 0);
         mCurrentPosition = getIntent().getIntExtra("currentPosition", 0);
@@ -25,5 +33,14 @@ public class PlayActivity extends AppCompatActivity {
 
         Log.d(TAG, ">> ulr " + mUrl + ", mStreamType " + mStreamType + ", mCurrentPosition " + mCurrentPosition);
         Log.d(TAG, ">> video " + mVideo);
+        //init player
+        mVideoView = bindViewId(R.id.video_view);
+        IjkMediaPlayer.loadLibrariesOnce(null);
+        IjkMediaPlayer.native_profileBegin("libijkplayer.so");
+    }
+
+    @Override
+    protected void initData() {
+
     }
 }
